a3fc32da764f7445173c0332e5c820d0f975a4aa
zhangzc <347328331@qq.com>
2016-09-11 17:27:01 +0800

    demo

--- src/main/java/cn/zichao/sy/HelloSpringBootApplication.java
+++ src/main/java/cn/zichao/sy/HelloSpringBootApplication.java
@@ -1,6 +1,7 @@
 package cn.zichao.sy;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +10,7 @@ public class HelloSpringBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringBootApplication.class, args);
 	}
+	@RequestMapping("")
 	public  String hello()
 	{
 		return  "Hello Docker and Spring boot";
